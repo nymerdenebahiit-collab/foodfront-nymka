40aba7b887bd2f72636cb64245a39cf166c96296
Nymka <nymerdenebahiit@gmail.com>
2026-03-09 14:17:45 +0800

    the end

--- src/app/_constants/index.js
+++ src/app/_constants/index.js
@@ -1,2 +1,2 @@
 // export const BACK_END_URL = "http://localhost:1000";
-export const BACK_END_URL = "https://food-backend-7ozf.onrender.com";
+export const BACK_END_URL = "https://foodback-nymka.onrender.com";
